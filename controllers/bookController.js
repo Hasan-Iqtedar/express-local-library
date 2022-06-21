89c4fcdfe6218bc4d0f5d66579cc5d0a0ebf6433
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-21 18:43:46 +0500

    Update controller for home page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,44 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookInstance');
+
+var async = require('async');
 
 exports.index = function (req, res, next) {
-  res.send('NOT IMPLEMENTED YET: Site Home Page');
+  async.parallel(
+    {
+      book_count: function (callback) {
+        Book.countDocuments({}, callback);
+      },
+      book_instance_count: function (callback) {
+        BookInstance.find({}, callback).count();
+      },
+      book_instance_available_count: function (callback) {
+        BookInstance.find()
+          .where('status')
+          .equals('Available')
+          .count()
+          .exec(callback);
+      },
+      author_count: function (callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        console.log(err);
+      }
+      res.render('index', {
+        title: 'Local Library Home',
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 exports.book_list = function (req, res, next) {
