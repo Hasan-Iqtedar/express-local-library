a60c6f738ac429d07a254ec36ee42a10dee671ee
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-21 23:05:27 +0500

    Update controller for genre detail

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,3 +1,5 @@
+var async = require('async');
+var Book = require('../models/book');
 var Genre = require('../models/genre');
 
 exports.genre_list = function (req, res, next) {
@@ -12,7 +14,26 @@ exports.genre_list = function (req, res, next) {
 };
 
 exports.genre_detail = function (req, res, next) {
-  res.send('NOT IMPLEMENTED YET: Genre Detail ' + req.params.id);
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).sort({ title: 1 }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render('genre_detail', {
+        title: 'Genre Details',
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 exports.genre_create_get = function (req, res, next) {
