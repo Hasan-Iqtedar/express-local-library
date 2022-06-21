49d9407e570f0cdccbd45df15f74fa404d19e84a
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-21 20:36:42 +0500

    Update book instance list controller

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,7 +1,18 @@
 var BookInstance = require('../models/bookInstance');
 
 exports.bookInstance_list = function (req, res, next) {
-  res.send('NOT IMPLEMENTED YET: BookInstance List');
+  BookInstance.find({})
+    .populate('book')
+    .exec(function (err, results) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render('bookInstance_list', {
+        title: 'Book Instance List',
+        bookInstances_list: results,
+      });
+    });
 };
 
 exports.bookInstance_detail = function (req, res, next) {
