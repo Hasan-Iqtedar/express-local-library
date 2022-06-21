e8eb1a3342c0ec6011912598bfce518b06432222
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-21 20:45:06 +0500

    Format dates using luxon

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
-
 var Schema = mongoose.Schema;
+
+const { DateTime } = require('luxon');
 
 var BookInstanceSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
@@ -16,6 +17,10 @@ var BookInstanceSchema = new Schema({
 
 BookInstanceSchema.virtual('url').get(function () {
   return '/catalog/bookInstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
