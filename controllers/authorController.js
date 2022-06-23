adfe25ec81bbc6bbb360255019ff013f24efc7f9
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-23 20:19:54 +0500

    Update controllers and views to delete author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -65,10 +65,12 @@ exports.author_create_post = [
     .withMessage('Family name has non-alphanumeric characters'),
   body('date_of_birth', 'Invalid date of birth')
     .optional({ checkFalsy: true })
-    .isISO8601().toDate(),
+    .isISO8601()
+    .toDate(),
   body('date_of_death', 'Invalid date of birth')
     .optional({ checkFalsy: true })
-    .isISO8601().toDate(),
+    .isISO8601()
+    .toDate(),
 
   (req, res, next) => {
     var errors = validationResult(req);
@@ -99,11 +101,62 @@ exports.author_create_post = [
 ];
 
 exports.author_delete_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED YET: Author Delete Get');
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      author_books: function (callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        res.redirect('/catalog/authors');
+      }
+      res.render('author_delete', {
+        title: 'Delete Author',
+        author: results.author,
+        author_books: results.author_books,
+      });
+    }
+  );
 };
 
 exports.author_delete_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED YET: Author Delete Post');
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.body.authorId).exec(callback);
+      },
+      author_books: function (callback) {
+        Book.find({ author: req.body.authorId }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author_books.length > 0) {
+        res.render('author_delete', {
+          title: 'Delete Author',
+          author: results.author,
+          author_books: results.author_books,
+        });
+        return;
+      } else {
+        Author.findByIdAndRemove(req.body.authorId, function (err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect('/catalog/authors');
+        });
+      }
+    }
+  );
 };
 
 exports.author_update_get = function (req, res, next) {
