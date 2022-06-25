30bae73657af53188fd6b814f4253405061b1e34
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-25 15:14:43 +0500

    Ready for production

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
-var mongoDB =
+var dev_db_url =
   'mongodb+srv://admin:1177@cluster0.4kgzavu.mongodb.net/?retryWrites=true&w=majority';
+
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
@@ -22,10 +26,13 @@ db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
